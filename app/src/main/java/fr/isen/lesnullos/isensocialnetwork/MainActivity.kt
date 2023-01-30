16d1b743852a2a04759626cf0b2fe491e9f36fd3
Francois <francois.benet@isen.yncrea.fr>
2023-01-30 11:43:41 +0100

    "test firebase"

--- app/src/main/java/fr/isen/lesnullos/isensocialnetwork/MainActivity.kt
+++ app/src/main/java/fr/isen/lesnullos/isensocialnetwork/MainActivity.kt
@@ -2,10 +2,21 @@ package fr.isen.lesnullos.isensocialnetwork
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        findViewById<Button>(R.id.button).setOnClickListener{
+            val database = Firebase.database
+            val myRef = database.getReference("message")
+            myRef.setValue("Hello, World!")
+        }
     }
 }
