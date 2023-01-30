2ad5cc9d1e75eea0e1c786bc8acbd515e0fb5038
Francois <francois.benet@isen.yncrea.fr>
2023-01-30 13:04:40 +0100

    "base firebase en place"

--- app/src/main/java/fr/isen/lesnullos/isensocialnetwork/MainActivity.kt
+++ app/src/main/java/fr/isen/lesnullos/isensocialnetwork/MainActivity.kt
@@ -2,8 +2,14 @@ package fr.isen.lesnullos.isensocialnetwork
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +17,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //lecture bdd
 
+        Firebase.database.getReference("message").addValueEventListener(object :
+            ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val value = snapshot.getValue<String>()
+                Log.d("TAG", "Value is: $value")
+                findViewById<TextView>(R.id.textView).text = value
+            }
 
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.d("TAG", "Failed to read value.", error.toException())
+            }
+        })
+
+        //ecriture bdd
         findViewById<Button>(R.id.button).setOnClickListener{
             val database = Firebase.database
             val myRef = database.getReference("message")
