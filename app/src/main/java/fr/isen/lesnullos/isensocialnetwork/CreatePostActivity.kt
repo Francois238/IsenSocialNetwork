4d152958fbe9f9f8f34dfcf5a9c95a7cf709d880
Drago164 <hugo.savy2001@gmail.com>
2023-01-30 15:58:34 +0100

    import des fichiers xml .png

---
+++ app/src/main/java/fr/isen/lesnullos/isensocialnetwork/CreatePostActivity.kt
@@ -0,0 +1,23 @@
+package fr.isen.lesnullos.isensocialnetwork
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreatePostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_post)
+
+        //bouton document
+
+        //bouton image
+
+        //bouton quitter -> retour sur fil
+
+        //bouton publier
+
+        //recuperer le texte user
+
+
+    }
+}
