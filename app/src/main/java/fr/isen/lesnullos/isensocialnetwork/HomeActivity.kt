124af4071b7d0951452d2dab52af2930bcc46297
Francois <francois.benet@isen.yncrea.fr>
2023-01-31 16:26:30 +0100

    "log in marche"

--- app/src/main/java/fr/isen/lesnullos/isensocialnetwork/HomeActivity.kt
+++ app/src/main/java/fr/isen/lesnullos/isensocialnetwork/HomeActivity.kt
@@ -1,26 +1,30 @@
 package fr.isen.lesnullos.isensocialnetwork
 
 import android.content.ContentValues
-import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
-import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import fr.isen.lesnullos.isensocialnetwork.databinding.ActivityHomeBinding
 
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
 
@@ -35,8 +39,8 @@ class HomeActivity : AppCompatActivity() {
         binding.btnRegisterAccont.setOnClickListener {
             Toast.makeText(this, "Click Register", Toast.LENGTH_SHORT).show()
 
-            val intent = Intent(this@HomeActivity, ProfileFormActivity::class.java)
-            startActivity(intent);
+           // val intent = Intent(this@HomeActivity, ProfileFormActivity::class.java)
+          //  startActivity(intent);
         }
 
         binding.forgetpassword.setOnClickListener {
@@ -56,6 +60,29 @@ class HomeActivity : AppCompatActivity() {
         val email = emailEditText.text.toString()
         val password = passwordEditText.text.toString()
 
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    //val id = user?.uid
+                    //val sharedPreferences = this.getSharedPreferences("user_id", Context.MODE_PRIVATE)
+                    // val editor = sharedPreferences.edit()
+                    // editor.putString("user_id", user?.uid)
+                    // editor.apply()
+                    Log.d(ContentValues.TAG, "L'utilisateur est connecté: $user")
+                    Toast.makeText(this, "CLick Login", Toast.LENGTH_SHORT).show();
+                    val intent = Intent(this@HomeActivity, CreatePostActivity::class.java)
+                    startActivity(intent);
+                } else {
+                    Log.e(ContentValues.TAG, "Erreur de connexion: ${task.exception}")
+                    // Affichez un message d'erreur en cas d'échec
+                    val errorCode = (task.exception as FirebaseAuthException).errorCode
+                    Log.e(ContentValues.TAG, "Code d'erreur: $errorCode")
+                }
+            }
+
+
+        /*
         // Envoyez les informations d'identification de l'utilisateur à Firebase pour la vérification
         val auth = FirebaseAuth.getInstance(FirebaseApp.getInstance("firebase"))
         auth.signInWithEmailAndPassword(email, password)
@@ -77,7 +104,9 @@ class HomeActivity : AppCompatActivity() {
                     val errorCode = (task.exception as FirebaseAuthException).errorCode
                     Log.e(ContentValues.TAG, "Code d'erreur: $errorCode")
                 }
-            }
+            }*/
+
+
     }
 
 }
